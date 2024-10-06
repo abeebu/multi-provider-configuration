d99f16ff74c64dc19104fb135896123e920e98f4
Abeeb Amoo <abeebamoo@microsoft.com>
2024-10-06 13:39:15 +0100

    remove print statement

--- src/app.py
+++ src/app.py
@@ -1,11 +1,13 @@
 import logging
+import sys
 from src.configuration.configuration import Configuration
 from src.configuration.configuration import Configuration
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 if __name__ == "__main__":
     # Should return "Alan Turing"
     random_config_value = Configuration.get_value("RANDOM_CONFIG_KEY")
-    print(random_config_value)
     logger.info(random_config_value)
